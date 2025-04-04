479f88f1e30c118d41244a298647dd9f93074dc9
LeonardoOzima <leonardojunozima@gmail.com>
2025-04-04 01:41:58 -0300

    Screen(Home): Added useQuery to handle requisitions, modified home to use useQuery.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 import "./App.css";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Screens/Home/Home";
 import About from "./Screens/About/About";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
---
+++ frontend/src/Screens/Home/Components/UserLine.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import UserType from "../Interfaces/UserTypes";
+import { UserBackendType } from "../Interfaces/UserTypes";
+import { FaRegTrashAlt } from "react-icons/fa";
+import api from "../../../Services/api";
+
+export default function UserLine(user: UserBackendType) {
+  async function handleDeleteUser(id: string) {
+    await api.delete(`/users/${id}`);
+    console.log("Usuário deletado com sucesso");
+  }
+
+  return (
+    <>
+      <div className="bg-slate-700 text-white flex flex-row p-4 rounded-lg">
+        <div className="flex flex-col p-4 justify-center align-middle">
+          {" "}
+          <span>Nome </span>
+          <span>Idade </span>
+          <span>Email</span>
+        </div>
+        <div className="flex flex-col px-4 bg-white text-black justify-center align-middle">
+          <h1> {user.name}</h1>
+          <h1> {user.age} </h1>
+          <h1> {user.email} </h1>
+        </div>
+        <div className="bg-white text-black flex flex-col align-middle justify-center">
+          <button
+            className="flex align-middle justify-center 
+          bg-gray-500 p-4 rounded-full w-12 h-12 items-center"
+          >
+            Editar
+          </button>
+          <button
+            onClick={() => handleDeleteUser(user.id)}
+            className="text-red-800 hover:text-white
+            bg-slate-300 hover:bg-red-800 rounded-full
+            transition-all duration-300 ease-linear
+             flex align-middle justify-center w-12 h-12 items-center"
+          >
+            <FaRegTrashAlt size={20}></FaRegTrashAlt>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
